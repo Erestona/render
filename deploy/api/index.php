f7219f438b9ea364d2e8c69b82d405b39c90053b
Tanguy <pierrontanguy@gmail.com>
2023-11-24 15:01:13 +0100

    php base

---
+++ deploy/api/index.php
@@ -0,0 +1,19 @@
+<?php
+	use Tuupola\Middleware\HttpBasicAuthentication;
+	use Slim\Factory\AppFactory;
+
+	require __DIR__ . '/../vendor/autoload.php';
+	require_once __DIR__ . '/../bootstrap.php';
+	
+	$app = AppFactory::create();
+
+	require_once __DIR__ . '/middleware.php';
+	require_once __DIR__ . '/controller.php';
+	require_once __DIR__ . '/route.php';
+	
+	
+	// Chargement du Middleware
+	$app->add(new Tuupola\Middleware\JwtAuthentication($options));
+	
+	// Run app
+	$app->run();
