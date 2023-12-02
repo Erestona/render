3558e2dd9fa06ea147614a3db684122e90c5a349
Tanguy <pierrontanguy@gmail.com>
2023-12-02 16:11:36 +0100

    updated login adn pwd

--- deploy/api/controller.php
+++ deploy/api/controller.php
@@ -47,7 +47,23 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 	// API Nécessitant un Jwt valide
 	function getCatalogue (Request $request, Response $response, $args) {
-	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+	    $flux = '[{ "id": 1, "name": "Produit 1", "category": "Catégorie 1", "price": 10 },
+		{ "id": 2, "name": "Produit 2", "category": "Catégorie 1", "price": 11 },
+		{ "id": 3, "name": "Produit 3", "category": "Catégorie 1", "price": 12 },
+		{ "id": 4, "name": "Produit 4", "category": "Catégorie 1", "price": 13 },
+		{ "id": 5, "name": "Produit 5", "category": "Catégorie 1", "price": 14 },
+	
+		{ "id": 6, "name": "Produit 6", "category": "Catégorie 2", "price": 10 },
+		{ "id": 7, "name": "Produit 7", "category": "Catégorie 2", "price": 12 },
+		{ "id": 8, "name": "Produit 8", "category": "Catégorie 2", "price": 14 },
+		{ "id": 9, "name": "Produit 9", "category": "Catégorie 2", "price": 16 },
+		{ "id": 10, "name": "Produit 10", "category": "Catégorie 2", "price": 18 },
+		
+		{ "id": 11, "name": "Produit 11", "category": "Catégorie 3", "price": 13 },
+		{ "id": 12, "name": "Produit 12", "category": "Catégorie 3", "price": 16 },
+		{ "id": 13, "name": "Produit 13", "category": "Catégorie 3", "price": 19 },
+		{ "id": 14, "name": "Produit 14", "category": "Catégorie 3", "price": 21 },
+		{ "id": 15, "name": "Produit 15", "category": "Catégorie 3", "price": 24 }]';
 	    $data = json_decode($flux, true); 
 	    
 	    $response->getBody()->write(json_encode($data));
@@ -92,10 +108,10 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 	    $login = $body ['login'] ?? "";
 	    $pass = $body ['password'] ?? "";
 
-	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
+	    if ($login!="emma")   {
 		$err = true;
 	    }
-	    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$pass))  {
+	    if ($pass!="toto")  {
 		$err=true;
 	    }
 	    if (!$err) {
